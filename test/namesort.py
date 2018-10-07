7808bd50627aa036b451a8202f456d2c28fb6910
wangxiaoyu1996 <1205661200@qq.com>
2018-10-07 20:46:16 +0800

    add notes

--- test/namesort.py
+++ test/namesort.py
@@ -17,49 +17,49 @@
 import chardet
 import os,sys,re
 
-if os.access('nameout.txt', os.F_OK):
+if os.access('nameout.txt', os.F_OK):      # check file nameout.txt if exist then delete
     os.remove('nameout.txt')
 
-files = os.listdir(os.getcwd())
-nameout = open('nameout.txt', mode='w')
+files = os.listdir(os.getcwd())            # make a filename list of current workspace
+nameout = open('nameout.txt', mode='w')    # create a file nameout.txt in write-only mode
 namelist = []
 
 
 for filename in files:
 
-    if filename == 'namesort.py':
+    if filename == 'namesort.py':          # make the object except main program
         continue
     
-    namefile = open(filename,mode = 'r')
+    namefile = open(filename,mode = 'r')   # open one file and prepare to check and sort
     coding = chardet.detect(namefile.read(200))['encoding']
-    namefile.seek(0,0)
+    namefile.seek(0,0)                     # check encode type
    # print coding
    # exit()
     for names in namefile:
         namestore = []
-        names = names.decode(coding)
-        names = names.split()
+        names = names.decode(coding)       # decode the chinese character in files
+        names = names.split()              # split the names by blank space
         for name in names:
             if len(name) == 1:
-                namestore.append(name)
+                namestore.append(name)     # store the single character
             else:
                 if name in namelist:
                     pass
                 else:
-                    nameout.write(name.encode('utf-8'))
-                    nameout.write('\n')
-                    namelist.append(name)
+                     nameout.write(name.encode('utf-8'))
+                     nameout.write('\n')   # write normal names in nameout
+                     namelist.append(name) # add name to list in order to check duplicate
 
         if len(namestore) == 0 :
             pass
         elif (len(namestore)%2) == 0:
             for i in range(0,len(namestore), 2):
                 name = namestore[i] + namestore[i+1]
-                if name in namelist:
+                if name in namelist:       # link single character to double-character name
                     pass
                 else:
                     nameout.write(name.encode('utf-8'))
-                    nameout.write('\n')
+                    nameout.write('\n')    # write the result to nameout
                     namelist.append(name)
         else:
             print 'error'
@@ -67,6 +67,6 @@ for filename in files:
     namefile.close()
 
 
-nameout.close()
+nameout.close()                            # close the in and out files
 
 
